98e25b2d3166b2518f12739b3686526b7cd630c6
Khasan077 <alikulovkh7@gmail.com>
2024-01-13 00:37:12 +0500

    add url copy button click handler

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
+const elUrlShortner = document.querySelector(".url-shortener");
 const elUrlShortnerForm = document.querySelector(".js-url-shortner-form");
 const elUrlShortnerResult = document.querySelector(".url-shortner__results");
 
-elUrlShortnerForm.addEventListener("submit", function(evt) {
-    evt.preventDefault();
+if (elUrlShortnerForm) {
+    elUrlShortnerForm.addEventListener("submit", function(evt) {
+        evt.preventDefault();
+    
+        elUrlShortnerResult.classList.add('url-shortner__results--open');
+    
+    })    
+}
 
-    elUrlShortnerResult.classList.add('url-shortner__results--open');
-
-})
+if (elUrlShortner) {
+    elUrlShortner.addEventListener("click", function (evt) {
+        if (evt.target.matches(".js-copy-short-link-button")) {
+            
+            evt.target.textContent = "Copied";
+            evt.target.classList.add('url-shortner__copy-button--copied');
+            setTimeout(function name(params) {
+                evt.target.textContent = "Copy"
+                evt.target.classList.remove('url-shortner__copy-button--copied')
+            },1000)
+        }
+    }) 
+}
